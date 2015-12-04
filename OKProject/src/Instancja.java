97b92678a9c1bdc3878979468060da48f9d64c1f
Kamil <kpiotrowski@comea.pl>
2015-12-04 15:44:54 +0100

    +++

--- OKProject/src/Instancja.java
+++ OKProject/src/Instancja.java
@@ -16,7 +16,7 @@ public class Instancja implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	public List<Zadanie> listaZadan = new ArrayList<Zadanie>();
-	
+	public List<Przerwa> listaPrzerw = new ArrayList<Przerwa>();
 	
 	public Instancja(){
 		for(int i=0 ; i< Main.iloscZadan ; i++){
@@ -28,10 +28,13 @@ public class Instancja implements Serializable{
 	}
 	
 	public void wypiszInstanje(){
+		System.out.println("ZADANIA:");
 		for (int i=0;i< Main.iloscZadan; i++){
 			Zadanie z = listaZadan.get(i);
 			System.out.println("ID: "+z.id+" Czas G: "+z.czasGotowosci+"  Operacje: "+z.A.maszyna+"/"+z.A.czasTrwania+"  |  "+z.B.maszyna+"/"+z.B.czasTrwania  );
 		}
+		System.out.println("PRZERWY:");
+		
 		
 	}
 }
---
+++ OKProject/src/Przerwa.java
@@ -0,0 +1,26 @@
+
+public class Przerwa {
+
+	/**
+	 * Czas startu zadania
+	 */
+	public int czasStaru=-1;
+	/**
+	 * Czas konca zadania
+	 */
+	public int czasKonca=-1;
+	/**
+	 * Czas trwania zadania
+	 */
+	public int czasTrwania=0;
+	/**
+	 * ID
+	 */
+	public int id=0;
+	
+	public static int idGen=0;
+	
+	public Przerwa(){
+		
+	}
+}
